471b0752a14efb6eef6699609e6e1fd6356f74b2
fedbocol29 <fedbocol29@gmail.com>
2026-09-18 16:18:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -166,3 +166,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }, { passive: true });
 
 });
+body{
+  opacity:1;
+  transition:opacity .55s ease;
+}
+
+body.page-leaving{
+  opacity:0;
+}
+
+html{
+  scroll-behavior:smooth;
+}
